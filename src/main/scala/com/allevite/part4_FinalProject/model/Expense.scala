e3e7d169cf4850bf8a272c6fee0e41613a30b5e7
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-06-04 19:39:14 +0530

    Expense and ExpenseSpec

---
+++ src/main/scala/com/allevite/part4_FinalProject/model/Expense.scala
@@ -0,0 +1,110 @@
+package com.allevite.part4_FinalProject.model
+
+
+import cats.*
+import cats.data.*
+import cats.implicits.*
+import com.allevite.part4_FinalProject.app.Configuration.IsValid
+import com.allevite.part4_FinalProject.common.Validations.*
+
+import scala.collection.immutable.SortedSet
+
+/**
+ * Represents a single expense.
+ *
+ * The participants are the people among who this expense will be divided.
+ * Note that this set does not contain the payer.
+ *
+ * For example, assume Alice buys a pizza for 10 dollars to eat with Bob and Charly.
+ * In this case the expense information is as follows:
+ *
+ * - The payer is Alice
+ * - The amount paid is $10.00
+ * - The partipants are Bob and Charly
+ *
+ * @param payer the person who paid
+ * @param amount the amount paid
+ * @param participants the people involved in the expense (excluding the payer)
+ */
+class Expense private (
+    val payer: Person,
+    val amount: Money,
+    val participants: NonEmptySet[Person]
+) :
+
+  /**
+    * TODO #11: Divide this amount among the payer and the participants so each
+    * has the same debt for this expense.
+    *
+    * For simplicity we don't care about losing cents. For example, dividing 1 dollar
+    * among 3 participants should yield 33 cents of debt for each participant.
+    */
+  def amountByParticipant: Money =
+    amount
+      .divideBy(participants.length + 1)
+      .get
+
+
+object Expense :
+  /**
+   * Creates an instance of a Expense without performing validations.
+   * Should only be used in tests.
+   */
+  def unsafeCreate(
+      payer: Person,
+      amount: Money,
+      participants: List[Person]
+  ): Expense =
+    new Expense(
+      payer,
+      amount,
+      NonEmptySet.fromSetUnsafe(SortedSet.from(participants))
+    )
+
+  /**
+    * TODO #11b: Create a validated expense. The validations to perform are:
+    * - The participants list should not be empty
+    * - The payer should not be included in the participants
+    *
+    * Note: List's contains method will use == equality. Figure out a way
+    * to use the equality instance received in the implicit argument eqPerson
+    */
+  def create(
+      payer: Person,
+      amount: Money,
+      participants: List[Person]
+  )(using eqPerson: Eq[Person]): IsValid[Expense] =
+    (
+      nonEmptySet(participants),
+      Validated.condNec(
+        Foldable[List].forall(participants)(_ neqv payer),
+        payer,
+        "payer cannot be included in participants"
+      )
+    ).mapN { (ps, p) =>
+      new Expense(p, amount, ps)
+    }
+
+  /**
+    * TODO #9: Implement an Eq instance by comparing every field,
+    * using the corresponding Eq instance for each type
+    * (i.e.: Person, Money, NonEmptySet[Person]).
+    */
+  given eqExpense( using 
+                   eqPerson: Eq[Person],
+                   eqMoney: Eq[Money],
+                    eqParticipants: Eq[NonEmptySet[Person]]
+                 ): Eq[Expense] =
+    Eq.instance((e1, e2) =>
+       e1.payer === e2.payer && e1.amount === e2.amount && e1.participants === e2.participants
+    )
+
+  /**
+    * TODO #8: Implement a Show instance with the following format:
+    *
+    * Expense[Payer=Martin,Amount=$10.00,Participants=Bob,Susan]
+    */
+  given showExpense: Show[Expense] =
+    Show.show { e =>
+      s"Expense[Payer=${e.payer.show},Amount=${e.amount.show},Participants=${e.participants.mkString_(",")}]"
+    }
---
+++ src/test/scala/com/allevite/finalProject/model/ExpenseSpec.scala
@@ -0,0 +1,62 @@
+package com.allevite.finalProject.model
+
+import cats.data.Validated.Valid
+import cats.implicits.*
+import cats.kernel.laws.discipline.EqTests
+import com.allevite.finalProject.FpFinalSpec
+import com.allevite.part4_FinalProject.model.{Expense, Money, Person}
+import org.scalacheck.{Arbitrary, Gen}
+
+class ExpenseSpec extends FpFinalSpec :
+  test("create a valid expense") :
+    given g: Arbitrary[(Person, List[Person])] = Arbitrary :
+      for
+        payer <- implicitly[Arbitrary[Person]].arbitrary
+        participants <- Gen.nonEmptyListOf(
+          implicitly[Arbitrary[Person]].arbitrary.filter(_ != payer)
+        )
+      yield (payer, participants)
+
+
+    forAll : (money: Money, p: (Person, List[Person])) =>
+      assert(
+        Expense.create(p._1, money, p._2) === Valid(
+          Expense.unsafeCreate(p._1, money, p._2)
+        )
+      )
+
+
+
+  test("create an invalid expense with no participants") :
+    forAll : (money: Money, payer: Person) =>
+      assert(Expense.create(payer, money, List.empty[Person]).isInvalid)
+
+
+
+  test("create an invalid expense with payer included in participants") :
+    forAll : (money: Money, payer: Person, participants: List[Person]) =>
+      assert(Expense.create(payer, money, payer :: participants).isInvalid)
+
+
+
+  test("show an expense") :
+    val e: Expense = Expense.unsafeCreate(
+      Person.unsafeCreate("Martin"),
+      Money.unsafeCreate(1000),
+      List(Person.unsafeCreate("Susan"), Person.unsafeCreate("Bob"))
+    )
+    assert(
+      e.show eqv "Expense[Payer=Martin,Amount=$10.00,Participants=Bob,Susan]"
+    )
+
+
+  test("calculating amount by participant is correct up to 1 dollar tolerance") :
+    forAll : (expense: Expense) =>
+      val money = expense.amountByParticipant
+      val centsTol = 100
+      val noPeople = 1 + expense.participants.length
+      assert(expense.amount.cents - money.times(noPeople).cents <= centsTol)
+
+
+  // TODO #10: Add the missing typeclass tests for Eq
+  checkAll("Eq[Expense]", EqTests[Expense].eqv)
