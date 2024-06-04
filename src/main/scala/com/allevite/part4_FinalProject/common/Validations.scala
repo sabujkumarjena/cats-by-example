c470fd46f8771c9ca8d2b77ff4c74fb0b87b6826
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-06-04 18:26:56 +0530

    Validation and validation spec

---
+++ src/main/scala/com/allevite/part4_FinalProject/common/Validations.scala
@@ -0,0 +1,54 @@
+package com.allevite.part4_FinalProject.common
+
+import cats.Order
+import cats.data.{NonEmptyChain, NonEmptySet, Validated}
+import com.allevite.part4_FinalProject.app.Configuration.IsValid
+
+import scala.collection.immutable.SortedSet
+
+/**
+ * Set of simple validations that can be reused across the different models.
+ */
+object Validations :
+  /**
+   * TODO #1: Check that this String's length does not exceed the provided limit.
+   */
+  def maxLength(s: String, n: Int): IsValid[String] =
+    Validated.condNec(
+      s.length <= n,
+      s,
+      s"String should have length at most $n"
+    )
+
+  /**
+   * TODO #2: Turn this String into a validated double
+   */
+  def double(s: String): IsValid[Double] = Validated.fromOption(s.toDoubleOption, NonEmptyChain("Invalid double"))
+
+
+  /**
+   * Validates that a Double is >= 0
+   */
+  def nonNegative(x: Double): IsValid[Double] =
+    Validated.condNec(x >= 0, x, s"Double should be nonnegative")
+
+  /**
+   * Validates that a list is non-empty and converts it to a NonEmptySet.
+   */
+  def nonEmptySet[A: Order](list: List[A]): IsValid[NonEmptySet[A]] =
+    Validated.fromOption(
+      NonEmptySet.fromSet(SortedSet.from(list)(Order[A].toOrdering)),
+      NonEmptyChain("List should be non-empty")
+    )
+
+  /**
+   * Validates that a String is non-empty.
+   */
+  def nonEmptyString(s: String): IsValid[String] =
+    Validated.condNec(s.nonEmpty, s, "String should be non-empty")
+
+  /**
+   * Validates that a String only contains letters.
+   */
+  def allLetters(s: String): IsValid[String] =
+    Validated.condNec(s.forall(_.isLetter), s, "String should be all letters")
---
+++ src/test/scala/com/allevite/finalProject/common/ValidationsSpec.scala
@@ -0,0 +1,109 @@
+package com.allevite.finalProject.common
+
+import cats.Order
+import cats.data.NonEmptySet
+import cats.data.Validated.Valid
+import cats.implicits._
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+import scala.collection.immutable.SortedSet
+
+class ValidationsSpec extends AnyFunSuite with ScalaCheckDrivenPropertyChecks {
+  import com.allevite.part4_FinalProject.common.Validations._
+
+  test("valid doubles") {
+    forAll { (d: Double) =>
+      assert(double(d.toString) eqv Valid(d))
+    }
+  }
+
+  test("invalid doubles") {
+    implicit val g: Arbitrary[String] = Arbitrary(Gen.alphaStr)
+    forAll { (s: String) =>
+      assert(double(s).isInvalid)
+    }
+  }
+
+  test("valid nonNegative") {
+    implicit val g: Arbitrary[Double] =
+      Arbitrary(Gen.choose(0, Double.MaxValue))
+    forAll { (d: Double) =>
+      assert(nonNegative(d) eqv Valid(d))
+    }
+  }
+
+  test("invalid nonNegative") {
+    implicit val g: Arbitrary[Double] = Arbitrary(Gen.negNum[Double])
+    forAll { (d: Double) =>
+      assert(nonNegative(d).isInvalid)
+    }
+  }
+
+  test("valid nonEmptySet") {
+    implicit val g: Arbitrary[List[Int]] =
+      Arbitrary(Gen.nonEmptyListOf(Arbitrary.arbitrary[Int]))
+    forAll { (ints: List[Int]) =>
+      assert(
+        nonEmptySet(ints) eqv Valid(
+          NonEmptySet.fromSetUnsafe(SortedSet.from(ints)(Order[Int].toOrdering))
+        )
+      )
+    }
+  }
+
+  test("invalid nonEmptySet") {
+    assert(nonEmptySet[Int](Nil).isInvalid)
+  }
+
+  test("valid nonEmptyString") {
+    val g: Gen[String] = Arbitrary.arbitrary[String].suchThat(_.nonEmpty)
+    forAll(g) { (s: String) =>
+      assert(nonEmptyString(s) eqv Valid(s))
+    }
+  }
+
+  test("invalid nonEmptyString") {
+    assert(nonEmptyString("").isInvalid)
+  }
+
+  test("valid allLetters") {
+    forAll(Gen.alphaStr) { (s: String) =>
+      assert(allLetters(s) eqv Valid(s))
+    }
+  }
+
+  test("invalid allLetters") {
+    val g: Gen[String] = Gen.numStr.suchThat(_.nonEmpty)
+    forAll(g) { (s: String) =>
+      assert(allLetters(s).isInvalid)
+    }
+  }
+
+  test("valid maxLength") {
+    val g: Gen[(Int, String)] = for {
+      n <- Gen.choose(0, 1000)
+      m <- Gen.choose(0, n)
+      s <- Gen.stringOfN(m, Arbitrary.arbitrary[Char])
+    } yield (n, s)
+
+    forAll(g) {
+      case (n: Int, s: String) =>
+        assert(maxLength(s, n) eqv Valid(s))
+    }
+  }
+
+  test("invalid maxLength") {
+    val g: Gen[(Int, String)] = for {
+      n <- Gen.choose(0, 1000)
+      m <- Gen.choose(n + 1, n + 1000)
+      s <- Gen.stringOfN(m, Arbitrary.arbitrary[Char])
+    } yield (n, s)
+
+    forAll(g) {
+      case (n: Int, s: String) =>
+        assert(maxLength(s, n).isInvalid)
+    }
+  }
+}
