66fcefa39e427f0a5b92562f7f16115f70d3e9f4
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-06-04 20:17:58 +0530

    PersonService and PersonServiceSpec

---
+++ src/main/scala/com/allevite/part4_FinalProject/service/PersonService.scala
@@ -0,0 +1,91 @@
+package com.allevite.part4_FinalProject.service
+
+
+import cats.*
+import cats.data.*
+import cats.implicits.*
+import com.allevite.part4_FinalProject.model.Person
+import com.allevite.part4_FinalProject.service.PersonService.PersonOp
+
+/**
+ * A trait for adding, storing and then finding people.
+ *
+ * It uses the State monad to keep track of the people that the user of the application
+ * has added so far.
+ */
+trait PersonService :
+  import PersonService.*
+
+  val personService: PService
+
+  trait PService :
+    /**
+     * Finds a person with the given name, returning if it finds it in the state,
+     * or yielding None otherwise.
+     */
+    def findByName(name: String): PersonOp[Option[Person]]
+
+    /**
+     * Adds a person to the state.
+     */
+    def addPerson(person: Person): PersonOp[Unit]
+
+    /**
+     * Returns all the people in the state.
+     */
+    def getAllPeople(): PersonOp[List[Person]]
+
+
+
+object PersonService :
+  /**
+   * Represents a state containing people indexed by name.
+   *
+   * @param personByName a map with names as keys and the associated Person instance as values.
+   */
+  case class PersonState(personByName: Map[String, Person]) :
+    /**
+     * @return a new state with the given person added
+     */
+    def addPerson(person: Person): PersonState =
+      copy(personByName = personByName + (person.name -> person))
+
+
+  object PersonState :
+    /**
+     * A state with no people.
+     */
+    def empty: PersonState = PersonState(Map.empty)
+
+    given eqPersonState(using
+                               eqMap: Eq[Map[String, Person]]
+                              ): Eq[PersonState] =
+      Eq.instance((ps1, ps2) => ps1.personByName eqv ps2.personByName)
+
+
+  type PersonOp[A] = State[PersonState, A]
+
+/**
+ * TODO #22: Provide a LivePersonService with an implementation for PersonService.
+ */
+trait LivePersonService extends PersonService {
+  override val personService: PService = new PService {
+    /**
+     * Finds a person with the given name, returning it if it finds it in the state,
+     * or yielding None otherwise.
+     */
+    override def findByName(name: String): PersonOp[Option[Person]] =
+      State.inspect(_.personByName.get(name))
+    /**
+     * Adds a person to the state.
+     */
+    override def addPerson(person: Person): PersonOp[Unit] =
+      State.modify(_.addPerson(person))
+
+    /**
+     * Returns all the people in the state.
+     */
+    override def getAllPeople(): PersonOp[List[Person]] =
+      State.inspect(_.personByName.values.toList)
+  }
+}
---
+++ src/test/scala/com/allevite/finalProject/services/PersonServiceSpec.scala
@@ -0,0 +1,69 @@
+package com.allevite.finalProject.services
+
+import cats.implicits.*
+import cats.kernel.laws.discipline.EqTests
+import com.allevite.finalProject.FpFinalSpec
+import com.allevite.part4_FinalProject.model.Person
+import com.allevite.part4_FinalProject.service.PersonService.*
+import com.allevite.part4_FinalProject.service.{LivePersonService, PersonService}
+
+class PersonServiceSpec extends FpFinalSpec :
+  val service: PersonService#PService = new LivePersonService {}.personService
+
+  test("add person adds a person to the state") :
+    forAll { (person: Person, initialState: PersonState) =>
+      assert(
+        service.addPerson(person).runS(initialState).value eqv PersonState(
+          initialState.personByName + ((person.name, person))
+        )
+      )
+    }
+
+
+  test("findByName returns Some when person is found") :
+    forAll : (person: Person, initialState: PersonState) =>
+      val s = initialState.addPerson(person)
+      assert(
+        service
+          .findByName(person.name)
+          .run(s)
+          .value eqv (s, Some(person))
+      )
+
+
+
+  test("findByName returns None when person is not found") :
+    forAll : (person: Person, existingPersons: Map[String, Person]) =>
+      val initialState = PersonState(existingPersons - person.name)
+      assert(
+        service
+          .findByName(person.name)
+          .run(initialState)
+          .value eqv (initialState, None)
+      )
+
+
+
+  test("getAllPeople contains all people") :
+    forAll : (initialState: PersonState) =>
+      val (resultState, resultPeople) = service
+        .getAllPeople()
+        .run(initialState)
+        .value
+      assert(
+        (
+          resultState,
+          resultPeople.toSet
+        ) eqv (initialState, initialState.personByName.values.toSet)
+      )
+
+
+
+  test("PersonState#addPerson adds a person to the map") :
+    forAll : (p: Person, personState: PersonState) =>
+      assert(
+        personState
+          .addPerson(p)
+          .personByName eqv (personState.personByName + (p.name -> p))
+      )
+  checkAll("Eq[PersonState]", EqTests[PersonState].eqv)
